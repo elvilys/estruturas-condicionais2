b5b5a2683a941df757e75d82d507c7bb892e2e94
elvilys bolivar <elvilysbolivar@gmail.com>
2023-07-23 15:42:45 -0300

    finalizamos o projeto

--- index8.js
+++ index8.js
@@ -1,19 +1,26 @@
 const idadeAtleta = parseFloat(prompt("Digite sua idade:"));
 const pesoAtleta = parseFloat(prompt("Digite seu peso:"));
 
+if (idadeAtleta <=12) 
+    document.write("categoria infantil")
 
-if( idadeAtleta < 12){
-    document.write("Categoria infantil!")
-} else if ( idadeAtleta >=13 && idadeAtleta <=16) {
-    document.write ("ategoria Juvenil!")
-} else if ( idadeAtleta >=17 && idadeAtleta<=24){
-    document.write("Categoria Sênior")
-} else if (idadeAtleta >=24){
-    document.write("Categoria veterano")}
-
-
-if (pesoAtleta <=20){
-    document.write("Categoria infantil");
-} else if (pesoAtleta <= 40){
+else if (pesoAtleta <=30){
+    document.write("categoria infantil");
+ 
+    }
+if (idadeAtleta >=13 && idadeAtleta <=16){
     document.write("categoria juvenil")
+} else if (pesoAtleta > 40 && pesoAtleta <=45 ){
+    document.write("categoria peso juvenil");}
+ 
+if(idadeAtleta >=17 && idadeAtleta <=24){
+    document.write(" categoria  peso sênior")
+} else if (pesoAtleta >=45 && pesoAtleta <=60){
+
+} if (idadeAtleta > 24){
+    document.write("categoria veterano")
+} else if ( pesoAtleta >60){
+    document.write("categoria peso veterano")
+} else{
+    document.write("idade e peso invalido")
 }
